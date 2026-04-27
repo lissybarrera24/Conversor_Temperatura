d6df06f79d91c2a273e2f5ec8a60cecb3f442e80
lissybarrera24 <Lissybuezo1996@gmail.com>
2026-04-26 18:21:53 -0600

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,25 +1,32 @@
-
+import { useState } from "react";
 import Resultado from "./Resultado";
 
 function App() {
-  const temperatura = 25;
-  const unidad = "C";
+  const [temp, setTemp] = useState(25);
+  const [unidad, setUnidad] = useState("C");
 
-  const conversiones = ["C-F", "C-K", "F-K"];
+  const conversiones = ["C-F", "C-K", "F-C", "F-K", "K-C", "K-F"];
 
   return (
     <div style={{ textAlign: "center", marginTop: "40px" }}>
-      <h1>Conversor de Temperatura</h1>
+      <h1>🌡️ Conversor de Temperatura</h1>
 
-      <p>
-        Temperatura: {temperatura}° {unidad}
-      </p>
-
-      <Resultado
-        valor={temperatura}
-        tipo={unidad}
-        conversiones={conversiones}
+      <input
+        type="number"
+        value={temp}
+        onChange={(e) => setTemp(Number(e.target.value))}
       />
+
+      <select
+        value={unidad}
+        onChange={(e) => setUnidad(e.target.value)}
+      >
+        <option value="C">Celsius</option>
+        <option value="F">Fahrenheit</option>
+        <option value="K">Kelvin</option>
+      </select>
+
+      <Resultado valor={temp} tipo={unidad} conversiones={conversiones} />
     </div>
   );
 }
